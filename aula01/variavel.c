8a549ba515261f56d990a2dc5cc7e380f0538eec
RafaFelixSilva <165770069+RafaFelixSilva@users.noreply.github.com>
2024-08-14 20:53:33 +0100

    atividade01

---
+++ aula01/variavel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+float variavelGlobal;
+const int constante = 42;
+int main() {
+    int variavelLocal = 0;
+    variavelLocal = constante;
+    printf("Valor da constante: %d\n", constante);
+    printf("Valor da variavel global: %.2f\n", variavelGlobal);
+    printf("Valor da variavel local: %d\n", variavelLocal);
+    return 0;
+}
